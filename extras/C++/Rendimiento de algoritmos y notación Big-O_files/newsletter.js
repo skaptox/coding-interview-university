fe525961cb56ffd9be9db11058f3528e215a344d
Oscar Albornoz <oalbornoz08@gmail.com>
2017-08-22 01:26:30 -0400

    Marqued x

---
+++ extras/C++/Rendimiento de algoritmos y notación Big-O_files/newsletter.js
@@ -0,0 +1,50 @@
+﻿var urlSuscripcion = "http://mcs.krasis.es/client/addmodsuscriptor.asp";
+
+//puntero al formulario y prefijo en el nombre de los campos a validar
+function ValUser(prefix) {
+    var whitespace = ' \t\n\r';
+    var bvalue = true;
+    with (document.getElementById(prefix + "Email")) {
+        for (i = 0; i <= length; i++) {
+            if (whitespace.indexOf(value.charAt(i)) != -1) {
+                bvalue = false;
+            } else {
+                bvalue = true;
+                break;
+            }
+        }
+        if (bvalue == false || value == '') {
+            alert('El correo electrónico no puede estar en blanco');
+            focus();
+            return false;
+        }
+    }
+    
+  var expreReg = /\w+([-.]\w+)*@\w+([-.]\w+)*\.\w{2,}/gi;
+  var sEmail = document.getElementById(prefix + "Email").value;
+  var rExecute = sEmail.match(expreReg);
+  if (rExecute == null) {
+    alert("El correo introducido no tiene una sintaxis válida");
+    document.getElementById(prefix + "Email").focus();
+    return false;
+  }
+  
+  return true;
+}
+
+function submitSubscription(prefix) {
+    //Se valida el formulario
+    var res = ValUser(prefix);
+    //Si el resultado es "true", entonces se envía el formulario
+    if (res) {
+        //Se asignan nombres a los campos antes de enviarlos
+        document.getElementById(prefix + "Email").name = "Email";
+        document.getElementById(prefix + "LastName").name = "LastName";
+        document.getElementById(prefix + "Name").name = "Name";
+        document.getElementById(prefix + "NewsLetterId").name = "NewsLetterId";
+        //Se cambia el action
+        document.forms[0].action = urlSuscripcion;
+        //Se envía
+        document.forms[0].submit();
+    }
+}
---
+++ extras/C++/Rendimiento de algoritmos y notación Big-O_files/videosCampusMVP.js
@@ -0,0 +1,25 @@
+﻿//Inicialización de la API
+function YouTubeInit(){
+	YouTube.init(cargarListaVideos);
+
+	//Carga la lista de vídeos
+	function cargarListaVideos() {
+		YouTube.getUploadsListID("campusMVPes", function(uploadsID) {
+			//Recibe el id de la lista de vídeos subidos al canal
+			YouTube.getListaVideos(uploadsID, 3, mostrarVideos);
+		});
+	};
+
+	//Muestra los vídeos en el DIV de la página
+	function mostrarVideos(videos) {
+		if(videos && videos.length>0) {
+			var videoHTML = "";
+			for (i=0; i<videos.length; i++) {
+				var item = videos[i];
+				var videoid = "https://www.youtube.com/embed/"+item.videoId;
+				videoHTML += "<iframe width='300' height='200' src='"+videoid+"' frameborder='0' allowfullscreen></iframe><br/>"; 
+			}
+			document.getElementById("YouTubeRecentVideos").innerHTML = videoHTML;
+		}
+	};
+}
